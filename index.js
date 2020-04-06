50660475e9ca45627f6d564259542d35b35b941d
jokale <kalemajoanna@gmail.com>
2020-04-06 10:58:47 +0100

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = tutorials.map(function(tutorial) {
+//   return titleCased
+// };
+
+// const titleCased = tutorials.map(function(tutorial)) => {
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
-}
+	  return tutorials.map(str =>
+    str.split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+  )
+}	
